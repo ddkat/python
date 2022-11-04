a0f3fb3a3b6cf53fb926cbe7fa8b1650aa26112c
D Kat <k8959269@gmail.com>
2022-11-04 01:14:22 +0100

    counting words

---
+++ word_count.py
@@ -0,0 +1,8 @@
+def words_counter(text):
+    words = text.split()
+    words_count = len(words)
+    return words_count
+
+if __name__ == "__main__":
+    result = words_counter(input())
+    print(result)
