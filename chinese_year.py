36a3d73899f787733d533826d5834001f76a7a5f
D Kat <k8959269@gmail.com>
2022-11-08 22:39:15 +0100

    input fix

--- chinese_year.py
+++ chinese_year.py
@@ -1,3 +1,4 @@
+#return year's zodiac for input year
 def zodiac_year_qualifier(year):
     zodiacs = ['Monkey', 'Rooster', 'Dog', 'Pig', 'Rat', 'Ox', 'Tiger', 'Rabbit', 'Dragon', 'Snake', 'Horse', 'Goat']
     mod_year = year % 12
@@ -5,5 +6,5 @@ def zodiac_year_qualifier(year):
     return(result_zodiac)
 
 if  __name__ == "__main__":
-    year_user = int(input())
+    year_user = int(input('enter year'))
     print(zodiac_year_qualifier(year_user))
